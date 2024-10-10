80f97fb0078a19ec16dd73bd77600f6b8cd5c7ae
strong3879 <strong3879@gmail.com>
2024-10-10 21:19:26 +0900

    # data fetching - react ver

--- learn-nextjs14/app/(home)/page.tsx
+++ learn-nextjs14/app/(home)/page.tsx
@@ -1,13 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Navigation from "../../components/navigation";
 
-export const metadata = {
-  title: "Home",
-};
+export default function Page() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [moives, setMovies] = useState();
 
-export default function Tomato() {
+  const getMovies = async () => {
+    setIsLoading(true);
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
+      {isLoading ? " Loading... " : JSON.stringify(moives)}
     </div>
   );
 }
